fbf45ec5f4c3f91c557b4ff91b7047af9d047a8f
torguet <torguet@gmail.com>
2019-09-04 15:20:58 +0200

    Books features work.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {Book} from '../models/book.model';
+import {BooksService} from '../services/books.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  books: Book[];
+  bookSubscription: Subscription;
+
+  constructor(private booksService: BooksService,
+              private router: Router) { }
 
   ngOnInit() {
+    this.bookSubscription = this.booksService.bookSubject.subscribe((books: Book[]) => {
+      this.books = books;
+    });
+    this.booksService.getBooks();
+    this.booksService.emitBooks();
   }
 
+  onNewBook() {
+    this.router.navigate(['/books', 'new']);
+  }
+
+  onDeleteBook(book: Book) {
+    this.booksService.removeBook(book);
+  }
+
+  onViewBook(id: number) {
+    this.router.navigate(['/books', 'view', id]);
+  }
+
+  ngOnDestroy() {
+    this.bookSubscription.unsubscribe();
+  }
 }
